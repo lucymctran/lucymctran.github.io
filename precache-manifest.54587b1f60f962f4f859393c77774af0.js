5f4b006f8f84ce426d8ddaf29a56635fc7d8bc24
Lucy Tran <lucytran@lucys-mbp.lan>
2021-03-07 18:47:15 -0800

    Updates

---
+++ precache-manifest.54587b1f60f962f4f859393c77774af0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491a8b67b3de4ba305ca7a0aa97d9f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43b51a5f3583e1117084",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0d53748924b13c7aabb9",
+    "url": "/static/css/main.2f0f1bc1.chunk.css"
+  },
+  {
+    "revision": "43b51a5f3583e1117084",
+    "url": "/static/js/2.622e4f78.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/static/js/2.622e4f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d53748924b13c7aabb9",
+    "url": "/static/js/main.1d79516b.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "afe3c02e28859a17885ff14edbf2db9d",
+    "url": "/static/media/upside-down-heart.afe3c02e.svg"
+  }
+]);
